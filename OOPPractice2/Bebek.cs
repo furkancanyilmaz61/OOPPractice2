fd452c8310bf0bdb446b3286b051a558cfee0bd7
furkancanyilmaz61 <furkancaanyilmaz@gmail.com>
2025-01-14 17:13:59 +0300

    Add project files.

---
+++ OOPPractice2/Bebek.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Bebek
+{
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+    public int DogumTarihi { get; set; }
+
+    // Default Constructor
+    public Bebek()
+    {
+        Console.WriteLine("Ingaaaa");
+        DogumTarihi = DateTime.Now.Year; // Anlık yılı ata
+    }
+
+    // Parametreli Constructor
+    public Bebek(string ad, string soyad)
+    {
+        Console.WriteLine("Ingaaaa");
+        Ad = ad;
+        Soyad = soyad;
+        DogumTarihi = DateTime.Now.Year; // Anlık yılı ata
+    }
+}
---
+++ OOPPractice2/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Default constructor ile nesne oluşturma
+        Bebek bebek1 = new Bebek();
+        Console.WriteLine($"Ad: {bebek1.Ad}, Soyad: {bebek1.Soyad}, Doğum Tarihi: {bebek1.DogumTarihi}");
+
+        // Parametreli constructor ile nesne oluşturma
+        Bebek bebek2 = new Bebek("Furkancan", "Yilmaz");
+        Console.WriteLine($"Ad: {bebek2.Ad}, Soyad: {bebek2.Soyad}, Doğum Tarihi: {bebek2.DogumTarihi}");
+    }
+}
